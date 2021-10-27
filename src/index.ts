bc0961e209c6e7a507f21f52d8bda1f398fc5ac6
floer <floer.learner.01@gmail.com>
2021-10-27 10:25:12 +0800

    feat: add express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import { config } from "dotenv-safe";
+import express, { static as serve } from "express";
 
 // Initialize environment variables.
 config();
+
+express()
+  .use(serve("public"))
+  .listen(+(<string>process.env["PORT"]), <string>process.env["ADDRESS"]);
