24571c226dcf0f84609d985e0d2db85435c4aacc
floer <81667533+TMK04@users.noreply.github.com>
2022-08-21 20:46:14 +0800

    refactor(index.html): display all recipes

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,1 +1,23 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import type { Recipe } from "../requests/recipes.types";
+
+import Content from "../Components/Content";
+import RecipeCollection from "../Components/RecipeCollection";
+import { json } from "../requests/helpers";
+import { scanRecipes } from "../requests/recipes.requests";
+
 import "../common";
+
+scanRecipes()
+  .then(json<Recipe[]>)
+  .then((recipes) => {
+    createRoot(document.getElementById(Content.id) as HTMLElement).render(
+      <React.StrictMode>
+        <Content h1-children="All Recipes">
+          <RecipeCollection recipes={recipes} />
+        </Content>
+      </React.StrictMode>
+    );
+  });
